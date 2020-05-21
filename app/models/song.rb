ce167136c80fad1971fa86c2cfca7e5217fa5247
github username <you@example.com>
2020-05-20 22:28:58 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,25 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def song_notes=(contents)
+    contents.each do |c|
+      @notes = self.notes.build(content: c)
+    end
+    @notes
+  end
+
+  def song_notes
+    @notes
+  end
+
 end
